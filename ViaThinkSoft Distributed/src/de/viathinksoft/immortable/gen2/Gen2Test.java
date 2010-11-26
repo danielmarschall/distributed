62aaeb09722089d7981ce2074a7f31615f6e3ad9
Daniel Marschall <info@daniel-marschall.de>
2010-11-26 22:05:36 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/distributed@8 c0174927-3572-473e-823b-b6c625958400

--- ViaThinkSoft Distributed/src/de/viathinksoft/immortable/gen2/Gen2Test.java
+++ ViaThinkSoft Distributed/src/de/viathinksoft/immortable/gen2/Gen2Test.java
@@ -5,12 +5,12 @@ import java.math.BigInteger;
 
 import de.viathinksoft.immortable.gen2.math.ImmortableBase;
 
-public class M5_MAX_INT {
+public class Gen2Test {
 
 	public static void main(String[] args) throws IOException {
 		String val = "" + Integer.MAX_VALUE;
 		ImmortableWriter.writeImmortable(ImmortableBase.M5,
-				new BigInteger(val), "m5_" + val + ".txt", true);
+				new BigInteger(val), "gen2_m5_" + val + ".txt", true);
 	}
 
 }
