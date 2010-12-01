14f594ce9b6d6ef563bef88f7adb43e0f2fd9339
Daniel Marschall <info@daniel-marschall.de>
2010-12-01 20:42:47 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/distributed@35 c0174927-3572-473e-823b-b6c625958400

--- ViaThinkSoft Distributed/src/de/viathinksoft/immortable/gen2/ImmortableBase.java
+++
@@ -1,7 +0,0 @@
-package de.viathinksoft.immortable.gen2;
-
-public enum ImmortableBase {
-	
-	M5, M6
-
-}
--- ViaThinkSoft Distributed/src/de/viathinksoft/immortable/gen2/InvalidLengthException.java
+++
@@ -1,7 +0,0 @@
-package de.viathinksoft.immortable.gen2;
-
-public class InvalidLengthException extends Exception {
-
-	private static final long serialVersionUID = 559305281746485769L;
-
-}
