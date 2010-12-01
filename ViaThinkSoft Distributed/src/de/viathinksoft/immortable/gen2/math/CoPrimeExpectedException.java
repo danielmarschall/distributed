29eccfbeb66e44e368c607e9c9901a6a0cfd33c0
Daniel Marschall <info@daniel-marschall.de>
2010-12-01 20:28:46 +0000

    git-svn-id: https://svn.viathinksoft.com/svn/distributed@33 c0174927-3572-473e-823b-b6c625958400

--- ViaThinkSoft Distributed/src/de/viathinksoft/immortable/gen2/math/CoPrimeExpectedException.java
+++
@@ -1,7 +0,0 @@
-package de.viathinksoft.immortable.gen2.math;
-
-public class CoPrimeExpectedException extends CRTException {
-
-	private static final long serialVersionUID = 8804691690532130114L;
-
-}
